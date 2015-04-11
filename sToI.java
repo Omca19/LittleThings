bdab8cbd4a973b8407697d22d2462b886f8bfc7c
Omca19 <Omca19@users.noreply.github.com>
2015-04-11 21:52:36 +0100

    Create sToI.java

---
+++ sToI.java
@@ -0,0 +1,15 @@
+package LittleThings;
+/**
+ * If you're looking at this you're silly
+ *
+ */
+ 
+public class sToI {
+	public static void main(String[] args) {
+		String a = String.valueOf(2);   //integer to numeric string
+		int i = Integer.parseInt(a); //numeric string to an int
+		
+		System.out.print(a+"\n"+i);
+	}
+
+}
