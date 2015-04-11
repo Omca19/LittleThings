fa3ec1cd01857d2f10af30d7b0e2096ad96dbeb9
Omca19 <Omca19@users.noreply.github.com>
2015-04-11 21:56:37 +0100

    Create methodName.java

---
+++ methodName.java
@@ -0,0 +1,27 @@
+package LittleThings;
+
+/**
+ * Gets the name of the current running method
+ *	Prints:	main
+ *			    loop
+ *			    loop
+ *			    main
+ *
+ */
+
+public class methodName {
+	public static void main(String[] args) {
+		String methodName = Thread.currentThread().getStackTrace()[1].getMethodName();
+		System.out.println(methodName);
+		loop(2);
+		System.out.println(methodName);
+	}
+	
+	
+	static void loop(int k){
+		for (int i = 0; i < k; i++) {
+			String methodName = Thread.currentThread().getStackTrace()[1].getMethodName();
+			System.out.println(methodName);
+		}
+	}
+}
