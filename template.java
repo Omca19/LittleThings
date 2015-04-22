826a96063f58996e29ed29f2a55c602e2d9f5786
Omca19 <Omca19@users.noreply.github.com>
2015-04-22 18:55:03 +0100

    Update template.java

--- template.java
+++ template.java
@@ -3,10 +3,10 @@ import java.util.*;
 
 public class template {
 	public static void main(String[] args) {
-		Scanner scan =  new Scanner(System.in);
+		Scanner in =  new Scanner(System.in);
 		
-		String input = scan.nextLine();
-		int input1 = scan.nextInt();
+		String input = in.nextLine();
+		int input1 = in.nextInt();
 		
 		
 		
