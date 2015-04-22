6811b27b7c58e8d11a2174438fbb3f565d52776a
Omca19 <Omca19@users.noreply.github.com>
2015-04-22 18:54:11 +0100

    Create template.java

---
+++ template.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class template {
+	public static void main(String[] args) {
+		Scanner scan =  new Scanner(System.in);
+		
+		String input = scan.nextLine();
+		int input1 = scan.nextInt();
+		
+		
+		
+	}
+}
